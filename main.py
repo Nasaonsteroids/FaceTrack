3f6c2141536a52db0e57d7bc2ded4be236217d7b
Nasaonsteroids <119498887+Nasaonsteroids@users.noreply.github.com>
2025-10-08 11:34:59 +0200

    Update main.py
    Tracking features and visualizing it.

--- main.py
+++ main.py
@@ -45,6 +45,8 @@ def check_face(frame):
 
         
 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
 
 while True:
     #reads one frame from your video source
@@ -60,6 +62,20 @@ while True:
                 pass
 
         counter += 1
+        #inside your while loop, after you get frame:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+        for (x, y, w, h) in faces:
+            #Draw rectangle around face
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = frame[y:y+h, x:x+w]
+            eyes = eye_cascade.detectMultiScale(roi_gray)
+            for (ex, ey, ew, eh) in eyes:
+                #Draw rectangle around eyes
+                cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
 
         if face_match:
             cv2.putText(frame, "MATCH!", (20,450), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255,0),3)
